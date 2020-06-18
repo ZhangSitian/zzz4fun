6f1e614e0311be0fb0ac84a30b1bdc112c9f0dec
zhangsitian_bxbank <zhangsitian@aibank.com>
2020-06-18 20:18:45 +0800

    zzz

---
+++ fun-mybatis/src/main/java/com/zzz/learn/mybatis/dao/annotation/BaseDao.java
@@ -0,0 +1,28 @@
+/*
+ * 文件名：BaseDao.java
+ * 修改人：zhangsitian
+ * 修改时间：2019年2月13日
+ */
+ 
+package com.zzz.learn.mybatis.dao.annotation;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface BaseDao<T> {
+
+    int insert(T record);
+
+    T selectById(Long id);
+
+    List<T> selectByIds(Long... ids);
+
+    T selectByRecord(@Param("condition") T condition);
+
+    List<T> selectListByRecord(@Param("condition") T condition);
+
+    int updateById(@Param("record") T record);
+
+    int updateByRecord(@Param("condition") T condition, @Param("record") T record);
+}
